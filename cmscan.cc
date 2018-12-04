9201ca12fc7c431030e687dc4a1435bf807b11bb
erriya <yvan.vallois@gmail.com>
2018-12-04 08:54:47 +0100

    add command line options provided by boost

--- cmscan.cc
+++ cmscan.cc
@@ -1,91 +1,128 @@
-#include "cmscandetectorconstruction.hh"
-#include "cmscanactioninitialization.hh"
+#include <iostream>
 
+#include <boost/program_options.hpp>
+
+#include "G4UIterminal.hh"
 #include "G4RunManager.hh"
-
-#include "G4UImanager.hh"
 #include "FTFP_BERT.hh"
 #include "G4StepLimiterPhysics.hh"
-
 #include "G4VisExecutive.hh"
 #include "G4UIExecutive.hh"
 
-//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo.....
-//TODO fichier config
+#include "cmscandetectorconstruction.hh"
+#include "cmscanactioninitialization.hh"
+
+namespace po = boost::program_options;
+
+void conflicting_options(const po::variables_map& vm, const char* opt1, const char* opt2);
+
 int main(int argc,char** argv)
 {
+    try {
+        /// Parse program options
+        po::options_description description("Allowed options");
+        description.add_options()
+                ("help", "produce help message")
+                ("version", "output the version number")
+                ("config", po::value<std::string>(), "path/to/json/my_json.json")
+                ("batch,B", po::value<std::string>(), "Launch in batch mode")
+                ("interactive,I", "Launch in interactive mode")
+                ("gui,G", "Launch in gui mode");
 
-	// Detect interactive mode (if no arguments) and define UI session
-  	G4UIExecutive* ui_executive = nullptr;
-  	if ( argc == 1 ) {
+        po::variables_map vm;
+        po::store(po::command_line_parser(argc, argv).
+                options(description).run(), vm);
+        po::notify(vm);
 
-	    ui_executive = new G4UIExecutive(argc, argv );
-  	}
+        conflicting_options(vm, "batch", "interactive");
+        conflicting_options(vm, "batch", "gui");
+        conflicting_options(vm, "interactive", "gui");
 
-  	/// Choose the Random engine
-  	G4Random::setTheEngine(new CLHEP::RanecuEngine);
+        if (vm.count("help")) {
 
-  	/// Construct the default run manager
-    auto* runManager = new G4RunManager;
+            std::cout << "Usage: options_description [options]\n";
+            std::cout << description;
+            return 0;
+        }
 
-	/// Set mandatory initialization classes
-	runManager->SetUserInitialization(new CMScanDetectorConstruction());
+        if (vm.count("config"))
+            std::cout << "config file is " << vm["config"].as<std::string>() << "\n";
+        else
+            throw std::logic_error(std::string("Error : configuration file is missing"));
 
-	G4VModularPhysicsList* physicsList = new FTFP_BERT;
-  	physicsList->RegisterPhysics(new G4StepLimiterPhysics());
-  	runManager->SetUserInitialization(physicsList);
 
-  	runManager->SetUserInitialization(new CMScanActionInitialization("../data"));
+        /// Choose the Random engine
+        G4Random::setTheEngine(new CLHEP::RanecuEngine);
 
-  	///////////////////////////////////////////////////////////////////////////////////////////
+        /// Construct the default run manager
+        auto *run_manager = new G4RunManager;
 
-	G4cout<<"OK => declaration et initialisation\n"<<G4endl;
-	// in case something is wrong
-	if ( G4RunManager::GetRunManager()->GetUserPhysicsList() == nullptr) {
+        /// Set mandatory initialization classes
+        run_manager->SetUserInitialization(new CMScanDetectorConstruction());
+        G4VModularPhysicsList *physicsList = new FTFP_BERT;
+        physicsList->RegisterPhysics(new G4StepLimiterPhysics());
+        run_manager->SetUserInitialization(physicsList);
+        run_manager->SetUserInitialization(new CMScanActionInitialization("../data"));
+        run_manager->Initialize();
 
-		G4cerr << G4endl << " ***** ERROR ***** The PHYSICS LIST is not set ***** ERROR ***** " << G4endl;
-		exit (-1);
-	}
-	if ( G4RunManager::GetRunManager()->GetUserDetectorConstruction() == nullptr) {
+        /// in case something is wrong
+        if (G4RunManager::GetRunManager()->GetUserPhysicsList() == nullptr)
+            throw std::logic_error(std::string("Error :  The PHYSICS LIST is not set"));
 
-		G4cerr << G4endl << " ***** ERROR ***** The DETECTOR is not built ***** ERROR ***** " << G4endl;
-		exit (-1);
-	}
-	if ( G4RunManager::GetRunManager()->GetUserPrimaryGeneratorAction() == nullptr) {
+        if (G4RunManager::GetRunManager()->GetUserDetectorConstruction() == nullptr)
+            throw std::logic_error(std::string("Error :  The DETECTOR is not built"));
 
-		G4cerr << G4endl << " ***** ERROR ***** The Primary generator is not defined ***** ERROR ***** " << G4endl;
-		exit (-1);
-	}
+        if (G4RunManager::GetRunManager()->GetUserPrimaryGeneratorAction() == nullptr)
+            throw std::logic_error(std::string("Error :  The Primary generator is not defined"));
 
-	///////////////////////////////////////////////////////////////////////////////////////////
-	/// Initialize visualization
-	///////////////////////////////////////////////////////////////////////////////////////////
 
-	G4VisManager* visManager = new G4VisExecutive;
-	visManager->Initialize();
+        /// launcher
+        G4UImanager *ui_manager = G4UImanager::GetUIpointer();
+        if (vm.count("batch")){
 
-	// Get the pointer to the User Interface manager
-	G4UImanager* UImanager = G4UImanager::GetUIpointer();
+            std::cout << "file of commands : " << vm["batch"].as<std::string>() << std::endl;
+            std::string command = "/control/execute ";
+            std::string macro_file_name = vm["batch"].as<std::string>();
+            ui_manager->ApplyCommand(command + macro_file_name);
+        }
 
-	// Process macro or start UI session
-	if (ui_executive != nullptr) {
+        if (vm.count("interactive")){
 
-		// interactive mode
-		UImanager->ApplyCommand("/control/execute init_vis.mac");
-		ui_executive->SessionStart();
-		delete ui_executive;
-	}
-	else {
+            G4UIsession *session = new G4UIterminal;
+            session->SessionStart();
+            delete session;
+        }
 
-		// batch mode
-		G4String command = "/control/execute ";
-		std::string fileName = argv[1];
-		UImanager->ApplyCommand(command + fileName);
-	}
+        if (vm.count("gui")){
 
-	delete visManager;
-	delete runManager;
+            G4UIExecutive *ui_executive = new G4UIExecutive(argc, argv);
+            G4VisManager *vis_manager = new G4VisExecutive;
+            vis_manager->Initialize();
+            ui_manager->ApplyCommand("/control/execute init_vis.mac");
+            ui_executive->SessionStart();
+
+            delete ui_executive;
+            delete vis_manager;
+        }
+
+        delete run_manager;
+    }
+    catch(std::exception& e) {
+
+            std::cout << e.what() << "\n";
+            return 1;
+    }
+
 }
 
+/**
+ * @brief Function used to check that 'opt1' and 'opt2' are not specified at the same time.
+ * @param vm map which store input variables
+ * @param opt1 first input option
+ * @param opt2 second input option
+ */
+void conflicting_options(const po::variables_map& vm, const char* opt1, const char* opt2) {
 
-//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo.....
+    if (vm.count(opt1) && !vm[opt1].defaulted() && vm.count(opt2) && !vm[opt2].defaulted())
+        throw std::logic_error(std::string("Conflicting options '") + opt1 + "' and '" + opt2 + "'.");
+}
